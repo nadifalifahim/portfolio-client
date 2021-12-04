f4633181e73053f6943b7b1afc227111ee85c6e2
Nadif Ali Fahim <nadifalifahim@gmail.com>
2021-12-05 00:58:41 +0600

    add instant messaging box

--- src/Pages/Shared/InstantMessage/InstantMessage.js
+++ src/Pages/Shared/InstantMessage/InstantMessage.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InstantMessage.css";
 
 const InstantMessage = () => {
+  const [instantMessageOpen, setInstantMessageOpen] = useState(false);
   return (
     <div className="instantMessage-container">
-      <div className="instantMessage-circle">
-        <i className="far fa-comment-dots"></i>
+      <button
+        onClick={() => {
+          setInstantMessageOpen(!instantMessageOpen);
+        }}
+      >
+        <div className="instantMessage-circle">
+          <i className="far fa-comment-dots"></i>
+        </div>
+      </button>
+      {instantMessageOpen && (
         <div className="instantMessage-message">
           <input placeholder="Name" type="email" />
-          <textarea placeholder="Your Message"></textarea>
+          <textarea rows="8" placeholder="Your Message"></textarea>
         </div>
-      </div>
+      )}
     </div>
   );
 };
