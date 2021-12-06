f55f8bcaa99c5899acbebefb008d1e47084739a9
Nadif Ali Fahim <nadifalifahim@gmail.com>
2021-12-06 13:26:36 +0600

    updates contact form

--- src/Pages/Home/Contact/Contact.js
+++ src/Pages/Home/Contact/Contact.js
@@ -30,48 +30,53 @@ const Contact = () => {
     <div className="contact-container">
       <Fade>
         <div className="contact-content">
-          <h1>Get in touch</h1>
-          <p>
-            Thanks for taking the time to reach out. How can I help you today?
+          <p className="contact-heading">
+            Thanks for taking the time to reach out. <br /> How can I help you
+            today?
           </p>
           <div className="contact-items-container">
-            <div className="contact-left-items-container">
+            {/* <div className="contact-left-items-container">
               <div className="contact-left-items">
                 <i className="fas fa-map-marked-alt"></i>
                 <div>
-                  <h2>Reach me</h2>
+                  <h2>Address</h2>
                   <p>Dhaka Bangladesh</p>
                 </div>
               </div>
               <div className="contact-left-items">
                 <i className="fas fa-mobile-alt"></i>
                 <div>
-                  <h2>Call me</h2>
+                  <h2>Phone</h2>
                   <p>+8801775011022</p>
                 </div>
               </div>
               <div className="contact-left-items">
                 <i className="fas fa-paper-plane"></i>
                 <div>
-                  <h2>Mail me</h2>
+                  <h2>Email</h2>
                   <p>nadifalifahim@gmail.com</p>
                 </div>
               </div>
-            </div>
+            </div> */}
             <div className="contact-form-container">
               <form ref={form} onSubmit={handleContactSubmit}>
                 <div className="nameemail-container">
-                  <div>
+                  <div className="contact-form-content">
                     <label>Name</label> <br />
                     <input type="text" name="name" />
                   </div>
-                  <div>
-                    <label>Email*</label> <br />
+                  <div className="nameemail-space"></div>
+                  <div className="contact-form-content">
+                    <label>Email *</label> <br />
                     <input type="text" name="email" required />
                   </div>
                 </div>
-                <label>Your Message</label>
-                <textarea name="message" className="form-text-area" />
+                <br />
+                <br />
+                <div className="contact-form-content">
+                  <label>Message</label> <br />
+                  <textarea name="message" className="form-text-area" />
+                </div>
                 <button type="submit" className="form-submit-button">
                   Submit
                 </button>
