583b0d0f5453f76c1d7c85969e9b847f7cc87f8c
Nadif Ali Fahim <nadifalifahim@gmail.com>
2021-12-06 17:52:01 +0600

    updates project

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,6 +4,7 @@ import Contact from "../Contact/Contact";
 import TopBanner from "../TopBanner/TopBanner";
 import { Routes, Route } from "react-router-dom";
 import PageNotFound from "../../PageNotFound/PageNotFound";
+import Projects from "../Projects/Projects";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Routes>
         <Route exact path="/" element={<TopBanner />} />
         <Route exact path="/home" element={<TopBanner />} />
+        <Route exact path="/projects" element={<Projects />} />
         <Route exact path="/contact" element={<Contact />} />
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
---
+++ src/Pages/Home/Projects/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Projects.css";
+
+const Projects = () => {
+  return (
+    <div className="projects-container">
+      <h1>My Recent Work</h1>
+    </div>
+  );
+};
+
+export default Projects;
